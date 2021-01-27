884b5003105996072316edd2448f4f0796f7a1ce
ochikov <ochikov@users.noreply.github.com>
2021-01-27 15:05:59 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,192 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+import Web3Modal from 'web3modal';
+// @ts-ignore
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import Column from './components/Column';
+import Wrapper from './components/Wrapper';
+import Header from './components/Header';
+import Loader from './components/Loader';
+import ConnectButton from './components/ConnectButton';
+
+import { Web3Provider } from '@ethersproject/providers';
+import { getChainData } from './helpers/utilities';
+
+const SLayout = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  text-align: center;
+`;
+
+const SContent = styled(Wrapper)`
+  width: 100%;
+  height: 100%;
+  padding: 0 16px;
+`;
+
+const SContainer = styled.div`
+  height: 100%;
+  min-height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  word-break: break-word;
+`;
+
+const SLanding = styled(Column)`
+  height: 600px;
+`;
+
+// @ts-ignore
+const SBalances = styled(SLanding)`
+  height: 100%;
+  & h3 {
+    padding-top: 30px;
+  }
+`;
+
+interface IAppState {
+  fetching: boolean;
+  address: string;
+  provider: any;
+  library: any;
+  connected: boolean;
+  chainId: number;
+  pendingRequest: boolean;
+  result: any | null;
+  electionContract: any | null;
+  info: any | null;
+}
+
+const INITIAL_STATE: IAppState = {
+  fetching: false,
+  address: '',
+  provider: null,
+  library: null,
+  connected: false,
+  chainId: 1,
+  pendingRequest: false,
+  result: null,
+  electionContract: null,
+  info: null
+};
+
+class App extends React.Component<any, any> {
+  // @ts-ignore
+  public web3Modal: Web3Modal;
+  public state: IAppState;
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      ...INITIAL_STATE
+    };
+
+    this.web3Modal = new Web3Modal({
+      network: this.getNetwork(),
+      cacheProvider: true,
+      providerOptions: this.getProviderOptions()
+    });
+  }
+
+  public componentDidMount() {
+    if (this.web3Modal.cachedProvider) {
+      this.onConnect();
+    }
+  }
+
+  public onConnect = async () => {
+    const provider = await this.web3Modal.connect();
+
+    const library = new Web3Provider(provider);
+    const network = await library.getNetwork();
+
+    const address = provider.selectedAddress ? provider.selectedAddress : provider?.accounts[0];
+
+    await this.subscribeToProviderEvents(provider);
+
+    await this.setState({
+      provider,
+      library,
+      chainId: network.chainId,
+      address,
+      connected: true
+    });
+  };
+
+  public subscribeToProviderEvents = async (provider: any) => {
+    if (!provider.on) {
+      return;
+    }
+    provider.on("close", () => this.resetApp());
+    provider.on("accountsChanged", async (accounts: string[]) => {
+      await this.setState({ address: accounts[0] });
+    });
+
+    provider.on("networkChanged", async (networkId: number) => {
+      const library = new Web3Provider(provider);
+      const network = await library.getNetwork();
+      const chainId = network.chainId;
+
+      await this.setState({ chainId, library });
+    });
+  };
+
+  public getNetwork = () => getChainData(this.state.chainId).network;
+
+  public getProviderOptions = () => {
+    const providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          infuraId: process.env.REACT_APP_INFURA_ID
+        }
+      }
+    };
+    return providerOptions;
+  };
+
+  public resetApp = async () => {
+    await this.web3Modal.clearCachedProvider();
+    this.setState({ ...INITIAL_STATE });
+  };
+
+  public render = () => {
+    const {
+      address,
+      connected,
+      chainId,
+      fetching
+    } = this.state;
+    return (
+      <SLayout>
+        <Column maxWidth={1000} spanHeight>
+          <Header
+            connected={connected}
+            address={address}
+            chainId={chainId}
+            killSession={this.resetApp}
+          />
+          <SContent>
+            {fetching ? (
+              <Column center>
+                <SContainer>
+                  <Loader />
+                </SContainer>
+              </Column>
+            ) : (
+                <SLanding center>
+                  <ConnectButton onClick={this.onConnect} />
+                </SLanding>
+              )}
+          </SContent>
+        </Column>
+      </SLayout>
+    );
+  };
+}
+
+export default App;
